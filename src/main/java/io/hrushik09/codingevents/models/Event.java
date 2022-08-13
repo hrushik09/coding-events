1c2ab64cb7d680d40a70e950412adb9b71db0c0e
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-08-13 16:55:50 +0530

    Added id to events

--- src/main/java/io/hrushik09/codingevents/models/Event.java
+++ src/main/java/io/hrushik09/codingevents/models/Event.java
@@ -1,12 +1,22 @@
 package io.hrushik09.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
+    private int id;
+    private static int nextId = 1;
     private String name;
     private String description;
 
     public Event(String name, String description) {
+        this.id = nextId;
+        nextId++;
         this.name = name;
         this.description = description;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -28,5 +38,18 @@ public class Event {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
