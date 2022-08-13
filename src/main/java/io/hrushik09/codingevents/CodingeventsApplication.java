9b8f8b40cc94e612474411aded50e113e410f233
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-08-13 14:02:35 +0530

    Display form for adding new events

---
+++ src/main/java/io/hrushik09/codingevents/CodingeventsApplication.java
@@ -0,0 +1,13 @@
+package io.hrushik09.codingevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hrushik09/codingevents/CodingeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hrushik09.codingevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
