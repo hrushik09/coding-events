f2bb2f3a1cafacc697d1360c0c1a526e7f0a0c87
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2022-08-13 16:16:21 +0530

    Added description property for events

--- src/main/java/io/hrushik09/codingevents/models/Event.java
+++ src/main/java/io/hrushik09/codingevents/models/Event.java
@@ -2,9 +2,11 @@ package io.hrushik09.codingevents.models;
 
 public class Event {
     private String name;
+    private String description;
 
-    public Event(String name) {
+    public Event(String name, String description) {
         this.name = name;
+        this.description = description;
     }
 
     public String getName() {
@@ -13,6 +15,14 @@ public class Event {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
